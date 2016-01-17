79f3a6f01aa60d085d9419acee225137b8b7ca2f
Sean Walsh <seanmatthewwalsh@gmail.com>
2016-01-17 20:31:11 +0200

    initial

---
+++ src/ConfigTest/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace ConfigTest
+{
+    public class Settings
+    {
+        public string TestSetting { get; set; }
+    }
+
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json");
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            services.AddOptions();
+            services.ConfigureOptions<Settings>(); // errors here!
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IOptions<Settings> settings)
+        {
+            System.Diagnostics.Debug.Write(settings.Value.TestSetting); // test value here
+            app.UseIISPlatformHandler();
+
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup<Startup>()
+                .Build();
+
+            application.Run();
+        }
+    }
+}
